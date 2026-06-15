816db40409a51c412c1c97e4a853f32f9a160d2f
Pierre Falconnier <pierrefalco@hotmail.fr>
2026-06-15 18:56:56 +0200

    reverted

--- src/jomo/__init__.py
+++ src/jomo/__init__.py
@@ -1,3 +1,14 @@
 import logging
+import sys
+from pathlib import Path
 
 logging.getLogger().setLevel(logging.INFO)
+
+
+# Automatically find where src/jomo/ is installed in site-packages
+_jomo_root = str(Path(__file__).resolve().parent)
+
+# Append src/jomo directly to sys.path at runtime
+# This makes internal absolute imports like `from models...` findable!
+if _jomo_root not in sys.path:
+    sys.path.insert(0, _jomo_root)
